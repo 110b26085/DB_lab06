034e2ba752b0391b1c2b1675684820b47393b6a3
110b26085 <115145634+110b26085@users.noreply.github.com>
2024-05-18 19:58:53 +0800

    Add files via upload

---
+++ C06_04.sql
@@ -0,0 +1,21 @@
+CREATE TABLE employee
+(employee_id  varchar(10) NOT NULL,
+employee_Name varchar(50) NOT NULL,
+employee_Add varchar(50) NOT NULL,
+Phone_home  char(10),
+PRIMARY KEY (employee_id)
+)
+START TRANSACTION;
+INSERT INTO employee(employee_id, employee_Name, employee_Add, Phone_home)VALUES('s001', '林小明', '台南市東區崇明路110號', '06-2563215');
+INSERT INTO employee(employee_id, employee_Name, employee_Add, Phone_home)VALUES('s002', '王大三', '台南市中西區自強一路二巷10號', '06-3556960');
+INSERT INTO employee(employee_id, employee_Name, employee_Add, Phone_home)VALUES('s003', '張小燕', '台中市東區忠明210號', '04-7778899');
+INSERT INTO employee(employee_id, employee_Name, employee_Add, Phone_home)VALUES('s004', '吳國林', '台北市中正區愛國西路二段250號', '02-6935487');
+COMMIT;
+SELECT * FROM employee;
+(1)
+SELECT *
+  FROM employee
+ WHERE employee_Add LIKE '台南市東區%';
+
+(2)
+    
